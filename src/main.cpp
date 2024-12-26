08f90b7e96784f9a7ac15027e29fd32f51c499ef
sushanthsk2004 <sushanthsk2004@berkeley.edu>
2024-12-25 23:14:51 -0800

    Initial files

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "gpu.hpp"
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+
+#ifdef USE_CUDA
+    std::cout << "CUDA: On" << std::endl;
+    printCudaVersion();
+#else
+    std::cout << "CUDA: Off" << std::endl;
+#endif
+
+    return 0;
+}
