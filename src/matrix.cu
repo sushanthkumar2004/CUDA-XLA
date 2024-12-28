541be91476f3a5a4d867dc0b683e19f0c95c2563
sushanthsk2004 <sushanthsk2004@berkeley.edu>
2024-12-28 13:58:36 -0800

    Feat: Compute dot product

--- src/matrix.cu
+++ src/matrix.cu
@@ -1,7 +1,2 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <cuda.h>
-#include <cuda_device_runtime_api.h>
-
 constexpr int BLOCK_SIZE = 512;
 
--- src/vec.cu
+++ src/vec.cu
@@ -1,8 +1,3 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <cuda.h>
-#include <cuda_device_runtime_api.h>
-
 constexpr int BLOCK_SIZE = 512;
 
 // Absolute
